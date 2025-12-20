a3033aec491ff92c5d9ec10e5422580c4e255866
Nicholas Romanets <mielkeroman99@proton.me>
2025-12-21 00:38:20 +0200

    Delete OpenPyXl directory
    Cosmetic changes. Changed of First commit

--- OpenPyXl/starting.py
+++
@@ -1,7 +0,0 @@
-from openpyxl import Workbook
-from openpyxl.reader.excel import load_workbook
-
-wb = Workbook()
-wb = load_workbook(filename='../Source.xlsx')
-wb = wb.active
-print(wb['F2'].value)
